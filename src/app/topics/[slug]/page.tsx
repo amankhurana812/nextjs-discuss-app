35ad6f404fc3e0624c199ee52adb45118e2b2cee
Aman khurana <amankhruana812@gmail.com>
2024-11-07 12:34:10 +0530

    added comments functionality

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
+import PostList from "@/components/posts/post-list";
 import PostCreateForm from "@/components/posts/posts-create-form";
+import { fetchPostByTopicSlug } from "@/db/queries/post";
 
 interface TopicShowPageProps {
   params: Promise<{
@@ -13,6 +15,7 @@ export default async function TopicShowPage({ params }: TopicShowPageProps) {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostByTopicSlug(slug)} />
       </div>
 
       <div>
